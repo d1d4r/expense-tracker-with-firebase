df1ee570064b51f8373eede448cfa9974a78f386
didar <deddarnawzad@gmail.com>
2024-01-04 19:55:28 +0300

    add pie chart in dashbord view

---
+++ src/components/charts/Pie-chart.vue
@@ -0,0 +1,36 @@
+<template lang="">
+  <apexchart :options="chartOptions" :series="series"></apexchart>
+</template>
+<script setup>
+import { reactive } from "vue";
+
+const series = reactive([44, 55, 13, 43, 22]);
+const chartOptions = reactive({
+  chart: {
+    width: 380,
+    type: "donut",
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  responsive: [
+    {
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 200,
+        },
+        legend: {
+          show: false,
+        },
+      },
+    },
+  ],
+  legend: {
+    position: "right",
+    offsetY: 0,
+    height: 230,
+  },
+});
+</script>
+<style lang=""></style>
--- src/views/Dashbord.vue
+++ src/views/Dashbord.vue
@@ -3,7 +3,7 @@
     <v-row>
       <v-col>
         <v-sheet :height="80" :width="250" color="success" class="text-center">
-          <v-card-title >Card Title</v-card-title>
+          <v-card-title>Card Title</v-card-title>
           <v-card-subtitle>Card Subtitle</v-card-subtitle>
         </v-sheet>
       </v-col>
@@ -27,8 +27,9 @@
       </v-col>
     </v-row>
   </v-container>
+  <PieChart />
 </template>
-<script>
-export default {};
+<script setup>
+import PieChart from "@/components/charts/Pie-chart.vue";
 </script>
 <style lang=""></style>
