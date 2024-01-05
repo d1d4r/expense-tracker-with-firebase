a6b3aa9a0c98286355dbbcd176b83e54551ce109
didar <deddarnawzad@gmail.com>
2024-01-05 21:04:11 +0300

    md3 bluprint

--- src/main.js
+++ src/main.js
@@ -9,12 +9,16 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import VueApexCharts from "vue3-apexcharts";
+import { md3 } from "vuetify/blueprints";
+
+
 
 const app = createApp(App);
 
 const vuetify = createVuetify({
   components,
   directives,
+  blueprints: md3,
 });
 
 app.use(createPinia());
