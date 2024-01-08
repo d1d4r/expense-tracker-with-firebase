c5c3495d040bfb0b430002f185fb9e18cf9a858b
didar <deddarnawzad@gmail.com>
2024-01-08 22:45:32 +0300

    Refactor Transactions-tabel.vue component

--- src/components/tabels/Transactions-tabel.vue
+++ src/components/tabels/Transactions-tabel.vue
@@ -1,28 +1,61 @@
 <template>
-  <EasyDataTable :headers="headers" :items="state.transactions">
-    <template #loading v-if="true">
-      <p>Loading...</p>
+  <EasyDataTable
+    :headers="headers"
+    :items="state.transactions"
+    :loading="state.loading"
+  >
+    <template #loading>
+      <p>loading...</p>
+    </template>
+    <template #item-operation="item">
+      <div class="operation-wrapper">
+        <img
+          src="./images/delete.png"
+          class="operation-icon"
+          @click="deleteItem(item)"
+        />
+        <img
+          src="./images/edit.png"
+          class="operation-icon"
+          @click="editItem(item)"
+        />
+      </div>
     </template>
   </EasyDataTable>
 </template>
 
 <script setup>
 import { getAllTransactions } from "@/service/data/transactions/getAllTransactions";
-import { onMounted, reactive } from "vue";
+//import { count } from "firebase/firestore";
+import { onMounted, reactive, computed } from "vue";
 
 const state = reactive({
   transactions: [],
+
   loading: false,
   error: null,
 });
 
-const headers = [{ text: "Name", value: "name" }];
+const headers = computed(() => {
+  const computedHeaders = [];
+  state.transactions.forEach((transaction) => {
+    Object.keys(transaction).forEach((key) => {
+      if (!computedHeaders.includes(key)) {
+        computedHeaders.push({
+          text: key.toUpperCase(),
+          value: key,
+        });
+      }
+    });
+  });
+  return computedHeaders;
+});
 
 onMounted(async () => {
   state.loading = true;
   try {
     const transactionData = await getAllTransactions();
-    console.log(transactionData);
+
     state.transactions = transactionData;
   } catch (error) {
     state.error = error;
@@ -30,7 +63,6 @@ onMounted(async () => {
     state.loading = false;
   }
 });
-
 </script>
 <style>
 .customize-table {
